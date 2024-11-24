8d6ec29d15ad6ffc1ea7c7bec3201071edb2d8a7
topazz888 <88342935+topazz888@users.noreply.github.com>
2024-11-24 23:39:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
+const wheel = document.getElementById("wheel");
+const spinBtn = document.getElementById("spin-btn");
+const finalValue = document.getElementById("final-value");
+
+const rotationValues = [
+  { minDegree: 0, maxDegree: 35, value: "Classic & Signature Cocktail" },
+  { minDegree: 36, maxDegree: 71, value: "OnlyFun" },
+  { minDegree: 72, maxDegree: 107, value: "You did great this year, but Santa still not chose you :(" },
+  { minDegree: 108, maxDegree: 143, value: "Good Girls Swallow" },
+  { minDegree: 144, maxDegree: 179, value: "Whiskey 1 shot" },
+  { minDegree: 180, maxDegree: 215, value: "Money Shot" },
+  { minDegree: 216, maxDegree: 251, value: "No Luck, Sad :((" },
+  { minDegree: 252, maxDegree: 287, value: "Tequila 1 shot" },
+  { minDegree: 288, maxDegree: 323, value: "Take It All In" },
+  { minDegree: 324, maxDegree: 359, value: "กรอกโค้ด HOHOHO1000 \nเพื่อรับส่วนลด 1000 บาท\nสำหรับการจ้างงานขั้นต่ำ 2000 บาท " },
+];
+
+const data = [10, 10, 10, 10, 10, 10, 10, 10, 10, 10];
+
+const pieColors = [
+  "#58694E",
+  "#0D2501",
+  "#58694E",
+  "#0D2501",
+  "#58694E",
+  "#0D2501",
+  "#58694E",
+  "#0D2501",
+  "#58694E",
+  "#0D2501",
+];
+
 let myChart = new Chart(wheel, {
   plugins: [ChartDataLabels],
   type: "pie",
@@ -31,8 +63,8 @@ let myChart = new Chart(wheel, {
       },
       datalabels: {
         color: "#ffffff",
-        anchor: "center", // จัดข้อความให้อยู่ตรงกลางช่อง
-        align: "center",  // จัดแนวให้อยู่ตรงกลาง
+        anchor: "center",  // จัดข้อความให้อยู่ตรงกลาง
+        align: "center",   // จัดข้อความให้แนวนอนตรงกลาง
         font: { size: 13 },
         clip: false,
         textAlign: "center",
@@ -40,3 +72,5 @@ let myChart = new Chart(wheel, {
     },
   },
 });
+
+const valueGe
