1066bcb371fa405f81d7cf56c29f42cd0d1ab551
topazz888 <88342935+topazz888@users.noreply.github.com>
2024-11-25 02:01:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ let myChart = new Chart(wheel, {
         color: "#ffffff",
         anchor: "center",
         align: "center",
-        offset: 10,
+        offset: 20,
         clip: true,
         formatter: (value, context) => {
           let label = context.chart.data.labels[context.dataIndex];
