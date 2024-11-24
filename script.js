c5fff3b6b70e2fa6a0452ca421bd6062bb35c522
topazz888 <88342935+topazz888@users.noreply.github.com>
2024-11-25 04:09:44 +0700

    Update script.js

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ let myChart = new Chart(wheel, {
       datalabels: {
         color: "#ffffff",
         anchor: "start",
-        align: "center",
+        align: "end",
         offset: 200,
         clip: true,
         formatter: (value, context) => {
