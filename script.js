b74afa89d23e79bcfef37adede40d1643e9fd0ef
topazz888 <88342935+topazz888@users.noreply.github.com>
2024-11-25 10:22:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ let myChart = new Chart(wheel, {
        offset: (context) => {
       const chart = context.chart;
       const outerRadius = chart.outerRadius || chart.width / 2; // ขอบวงล้อ
-      return outerRadius - 150; // เพิ่มระยะจากขอบวงล้อ (เช่น 10px)
+      return outerRadius - 120; // เพิ่มระยะจากขอบวงล้อ (เช่น 10px)
     },
     clip: false,
         
